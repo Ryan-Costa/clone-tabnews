b30376082355361b3ff705c84af07184b1b58806
sousaryan1 <62930267+Ryan-Costa@users.noreply.github.com>
2025-10-14 17:53:37 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function Home() {
+    const [count, setCount] = useState(0)
+    return (
+        <div>
+            <h1>Teste</h1>
+        </div>
+    )
+}
+
+export default Home;
