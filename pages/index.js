8e65b5d489853820c2fe1de521d2d2e2c660951e
sousaryan1 <62930267+Ryan-Costa@users.noreply.github.com>
2025-10-15 14:28:30 +0000

    atualização da frase inicial

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
 import { useState } from "react";
 
 function Home() {
-    const [count, setCount] = useState(0)
     return (
-        <h1>Themis eu amo você!</h1>
+        <h1>Themis, eu amo você!</h1>
     )
 }
 
